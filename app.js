10bccf48ade5a3bfa3a86e02e512fef5d4fd717a
mrslnputra <mrslnputra@gmail.com>
2021-09-24 06:25:30 +0700

    final__

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const router = require('./routes')
 const session = require('express-session')
-const port = 3000
+const port = process.env.PORT || 5000
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
